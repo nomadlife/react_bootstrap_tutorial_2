402b65afc1e579b9f03a150176a75afe5df85310
nomadlife_u18 <junnu.shon@gmail.com>
2020-02-24 09:24:33 +0900

    Add Pages

---
+++ src/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Contact = () => (
+        <div>
+            <h2>Contact Page</h2>
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat perspiciatis pariatur odit soluta, odio natus, laudantium cumque itaque similique velit debitis doloremque iure. Eos, in sequi? Ea quaerat nesciunt odio nam deserunt praesentium provident. Provident tempore voluptatibus quia quo tempora odit ea veritatis architecto dolor ad rem, neque quaerat impedit!
+            </p>
+            
+        </div>
+    )
---
+++ src/NoMatch.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const NoMatch = () => (
+        <div>
+            <h2>NoMatch Page</h2>
+            <p>No Match</p>
+        </div>
+    )
